26787e2f4da4b5324d72601fb0d1a4c3e9d0930b
Bruno Miguel Morais <bm.morais21@gmail.com>
2018-10-20 15:02:19 -0300

    início do projeto spring

---
+++ src/main/java/br/com/academiadev/bumblebee/BumblebeeApplication.java
@@ -0,0 +1,12 @@
+package br.com.academiadev.bumblebee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumblebeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumblebeeApplication.class, args);
+	}
+}
