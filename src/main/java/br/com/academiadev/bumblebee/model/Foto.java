f66686751863ea6939a166698daf74c3a562b9ed
Bruno Miguel Morais <bm.morais21@gmail.com>
2018-10-26 16:09:26 -0300

    #19 - model de foto

---
+++ src/main/java/br/com/academiadev/bumblebee/model/Foto.java
@@ -0,0 +1,53 @@
+package br.com.academiadev.bumblebee.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Foto {
+
+    @Id
+    @GeneratedValue
+    private Long idFoto;
+
+    // TODO: verificar como salvar as imagens
+    @NotNull
+    @Size(min = 1, max = 45)
+    private String nome_arquivo;
+
+    @NotNull
+    private Boolean excluido = false;
+
+    public Foto(){
+
+    }
+
+    public Long getIdFoto() {
+        return idFoto;
+    }
+
+    public void setIdFoto(Long idFoto) {
+        this.idFoto = idFoto;
+    }
+
+    public String getNome_arquivo() {
+        return nome_arquivo;
+    }
+
+    public void setNome_arquivo(String nome_arquivo) {
+        this.nome_arquivo = nome_arquivo;
+    }
+
+    public Boolean getExcluido() {
+        return excluido;
+    }
+
+    public void setExcluido(Boolean excluido) {
+        this.excluido = excluido;
+    }
+
+
+}
