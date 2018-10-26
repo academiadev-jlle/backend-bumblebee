c3d7429f8ae9546a96bd119713d36104b814554d
Bruno Miguel Morais <bm.morais21@gmail.com>
2018-10-26 15:53:31 -0300

    #19 - model de cidade

---
+++ src/main/java/br/com/academiadev/bumblebee/model/Cidade.java
@@ -0,0 +1,64 @@
+package br.com.academiadev.bumblebee.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cidade {
+
+    @Id
+    @GeneratedValue
+    private Long idUf;
+
+    @NotNull
+    @Size(min = 1, max = 45)
+    private String nome;
+
+    @NotNull
+    private Boolean excluido = false;
+
+    @ManyToOne
+    private Uf uf;
+
+
+    public Cidade(){
+
+    }
+
+    public Long getIdUf() {
+        return idUf;
+    }
+
+    public void setIdUf(Long idUf) {
+        this.idUf = idUf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getExcluido() {
+        return excluido;
+    }
+
+    public void setExcluido(Boolean excluido) {
+        this.excluido = excluido;
+    }
+
+    public Uf getUf() {
+        return uf;
+    }
+
+    public void setUf(Uf uf) {
+        this.uf = uf;
+    }
+
+}
