f2b8c6a035fff8d08cdcdfcb87f607015a2ef004
viniciusbsi <ifc.vinicius@gmail.com>
2018-10-30 16:50:16 -0300

    #6 - endpoint de localização

---
+++ src/main/java/br/com/academiadev/bumblebee/endpoint/LocalizacaoEndpoint.java
@@ -0,0 +1,59 @@
+package br.com.academiadev.bumblebee.endpoint;
+
+import br.com.academiadev.bumblebee.model.Localizacao;
+import br.com.academiadev.bumblebee.repository.LocalizacaoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/localizacao")
+@Api(description = "Localizações")
+public class LocalizacaoEndpoint {
+
+    @Autowired
+    private LocalizacaoRepository repository;
+
+    @ApiOperation(value = "Retorna uma lista de localizações")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localizações retornados com sucesso")
+    })
+    @GetMapping
+    public List<Localizacao> buscarTodos() {
+        return repository.findAll();
+    }
+
+    @ApiOperation(value = "Retorna uma localização")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localizações encontrado com sucesso")
+    })
+    @GetMapping("/{id}")
+    public List<Localizacao> buscarPor(@PathVariable Long id) {
+        return repository.findAll();
+    }
+
+    @ApiOperation(value = "Cria uma localização")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localização criada com sucesso")
+    })
+
+    @PostMapping
+    public Localizacao criar(@RequestBody Localizacao localizacao) {
+        return repository.save(localizacao);
+    }
+
+    @ApiOperation(value = "Deleta uma localização")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localização deletada com sucesso")
+    })
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+}
