aae923d61a40b6e629bd4e0b6ca654a1c3b71b09
viniciusbsi <ifc.vinicius@gmail.com>
2018-10-30 16:31:08 -0300

    #7 - repository de pet

---
+++ src/main/java/br/com/academiadev/bumblebee/repository/PetRepository.java
@@ -0,0 +1,11 @@
+package br.com.academiadev.bumblebee.repository;
+
+import br.com.academiadev.bumblebee.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+}
