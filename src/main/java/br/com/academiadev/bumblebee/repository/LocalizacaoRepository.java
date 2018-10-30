05258b1b3e2031412e1e5ee2108fedd87ccd8b8b
viniciusbsi <ifc.vinicius@gmail.com>
2018-10-30 16:49:30 -0300

    #7 - repository de localização

--- src/main/java/br/com/academiadev/bumblebee/repository/LocalizacaoRepository.java
+++ src/main/java/br/com/academiadev/bumblebee/repository/LocalizacaoRepository.java
@@ -1,11 +1,9 @@
 package br.com.academiadev.bumblebee.repository;
 
-import br.com.academiadev.bumblebee.model.Pet;
+import br.com.academiadev.bumblebee.model.Localizacao;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-    public interface PetRepository extends JpaRepository<Pet, Long> {
-
+public interface LocalizacaoRepository extends JpaRepository<Localizacao, Long> {
 }
