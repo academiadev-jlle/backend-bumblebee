6da514cea642007f0ff2fc79b29af17eefb867bf
viniciusbsi <ifc.vinicius@gmail.com>
2018-11-06 23:09:06 -0200

    #35 - Criada classe UsuarioDTO #34 - Criada classe UsuarioMapper #33 - Refatorada a model Usuario

--- src/main/java/br/com/academiadev/bumblebee/repository/UsuarioRepository.java
+++ src/main/java/br/com/academiadev/bumblebee/repository/UsuarioRepository.java
@@ -2,7 +2,11 @@ package br.com.academiadev.bumblebee.repository;
 
 import br.com.academiadev.bumblebee.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByEmail(String email);
 
 }
