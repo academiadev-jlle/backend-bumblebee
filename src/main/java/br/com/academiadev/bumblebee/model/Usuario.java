a8cbe0a6eb924c97e2c476ee282502d3dd1b45cf
Bruno Miguel Morais <bm.morais21@gmail.com>
2018-11-10 13:33:21 -0200

    correções

--- src/main/java/br/com/academiadev/bumblebee/model/Usuario.java
+++ src/main/java/br/com/academiadev/bumblebee/model/Usuario.java
@@ -9,13 +9,10 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Collection;
 
 
 @AllArgsConstructor
@@ -30,7 +27,7 @@ import java.util.Collection;
                 "SET excluido = true " +
                 "WHERE id = ?")
 @Where(clause="excluido=false")
-public class Usuario extends EntidadeAuditavel<Long> implements UserDetails {
+public class Usuario extends EntidadeAuditavel<Long>{
 
     @NotNull()
     @Size(min = 3, max = 120)
@@ -46,48 +43,6 @@ public class Usuario extends EntidadeAuditavel<Long> implements UserDetails {
     @Size(min = 6, max = 16)
     @ApiModelProperty(example = "123456", name = "Senha")
     private String senha;
-
-    @Override
-    @ApiModelProperty(hidden = true)
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    @ApiModelProperty(hidden = true)
-    public String getPassword() {
-        return senha;
-    }
-
-    @Override
-    @ApiModelProperty(hidden = true)
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    @ApiModelProperty(hidden = true)
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    @ApiModelProperty(hidden = true)
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    @ApiModelProperty(hidden = true)
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    @ApiModelProperty(hidden = true)
-    public boolean isEnabled() {
-        return true;
-    }
 
     public Usuario(Long id) {
         this.id = id;
