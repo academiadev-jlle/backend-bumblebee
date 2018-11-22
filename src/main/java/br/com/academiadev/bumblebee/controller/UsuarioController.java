45f6e470ea3d9b92ac7033b9025c8d0f73466a10
Bruno Miguel <bm.morais21@gmail.com>
2018-11-22 00:09:25 -0200

    #18 - configuração básica de autenticação

--- src/main/java/br/com/academiadev/bumblebee/controller/UsuarioController.java
+++ src/main/java/br/com/academiadev/bumblebee/controller/UsuarioController.java
@@ -4,12 +4,12 @@ import br.com.academiadev.bumblebee.exception.ObjectNotFoundException;
 import br.com.academiadev.bumblebee.model.Usuario;
 import br.com.academiadev.bumblebee.repository.UsuarioRepository;
 import br.com.academiadev.bumblebee.service.UsuarioService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //@RestController
 //@RequestMapping("/usuario")
@@ -63,6 +63,15 @@ public class UsuarioController extends CrudControllerAbstrato<UsuarioService, Us
     @Autowired
     public UsuarioController(UsuarioService service) {
         super(service);
+    }
+
+    @Override
+    @ApiImplicitParams({ //
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token", required = true, dataType = "string", paramType = "header") //
+    })
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<Usuario> buscarTodos() {
+        return service.findAll();
     }
 
 
