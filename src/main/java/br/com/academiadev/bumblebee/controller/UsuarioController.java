54b2f98de3ae078319bc1ec4198a3ce26c6d83af
Bruno Miguel <bm.morais21@gmail.com>
2018-11-27 09:56:44 -0200

    #18 - adicionadno bcrypt na senha

--- src/main/java/br/com/academiadev/bumblebee/controller/UsuarioController.java
+++ src/main/java/br/com/academiadev/bumblebee/controller/UsuarioController.java
@@ -1,8 +1,6 @@
 package br.com.academiadev.bumblebee.controller;
 
-import br.com.academiadev.bumblebee.exception.ObjectNotFoundException;
 import br.com.academiadev.bumblebee.model.Usuario;
-import br.com.academiadev.bumblebee.repository.UsuarioRepository;
 import br.com.academiadev.bumblebee.service.UsuarioService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,50 +8,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-//@RestController
-//@RequestMapping("/usuario")
-//@Api(description = "Usuarios")
-//public class UsuarioController{
-//
-//    @Autowired
-//    private UsuarioRepository repository;
-//
-//    @ApiOperation(value = "Retorna uma usuario")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 201, message = "Usuario encontrado com sucesso")
-//    })
-//    @GetMapping("/{id}")
-//    public Usuario buscarPor(@PathVariable Long id) throws ObjectNotFoundException {
-//        return repository.findById(id)
-//                .orElseThrow(() -> new ObjectNotFoundException("Usuário com id " + id + " não encontrado"));
-//    }
-//
-//    @ApiOperation(value = "Cria um Usuário")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 201, message = "Usuário criado com sucesso")
-//    })
-//
-//    @PostMapping
-//    public Usuario criar(@RequestBody Usuario usuario) {
-//        return repository.save(usuario);
-//    }
-
-//    @ApiOperation(value = "Deleta um Usuário")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 201, message = "Usuario deletado com sucesso")
-//    })
-//    @DeleteMapping("/{id}")
-//    public Usuario deletar(@PathVariable Long id) throws ObjectNotFoundException {
-//        Usuario usuario = repository.findById(id)
-//                .orElseThrow(() -> new ObjectNotFoundException("Usuário com id " + id + " não encontrado"));
-//        usuario.setExcluido(Boolean.TRUE);
-//        repository.save(usuario);
-//        return usuario;
-//    }
-
-//}
-
 
 @RestController
 @RequestMapping(value = "/usuario")
