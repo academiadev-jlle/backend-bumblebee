89157448988eca6dbfc345b1e9379038be16f645
viniciusbsi <ifc.vinicius@gmail.com>
2018-11-27 21:03:21 -0200

    #57 - Alterado os controllers de Cidade, Localização e Pet para criar e retornar objetos utilizando DTOs

--- src/main/java/br/com/academiadev/bumblebee/controller/LocalizacaoController.java
+++ src/main/java/br/com/academiadev/bumblebee/controller/LocalizacaoController.java
@@ -1,19 +1,86 @@
 package br.com.academiadev.bumblebee.controller;
 
+import br.com.academiadev.bumblebee.dto.Localizacao.LocalizacaoDTO;
+import br.com.academiadev.bumblebee.dto.Localizacao.LocalizacaoDTOResponse;
+import br.com.academiadev.bumblebee.dto.Uf.UfDTO;
+import br.com.academiadev.bumblebee.dto.Uf.UfDTOResponse;
+import br.com.academiadev.bumblebee.exception.ObjectNotFoundException;
+import br.com.academiadev.bumblebee.mapper.LocalizacaoMapper;
+import br.com.academiadev.bumblebee.mapper.UfMapper;
 import br.com.academiadev.bumblebee.model.Localizacao;
+import br.com.academiadev.bumblebee.model.Uf;
+import br.com.academiadev.bumblebee.repository.LocalizacaoRepository;
+import br.com.academiadev.bumblebee.repository.UfRepository;
 import br.com.academiadev.bumblebee.service.LocalizacaoService;
+import br.com.academiadev.bumblebee.service.UfService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/localizacao")
 @Api(description = "Localizações")
-public class LocalizacaoController extends CrudControllerAbstrato<LocalizacaoService, Localizacao, Long>{
+public class LocalizacaoController{
 
     @Autowired
-    public LocalizacaoController(LocalizacaoService service){
-        super(service);
+    private LocalizacaoRepository localizacaoRepository;
+
+    @Autowired
+    private LocalizacaoMapper localizacaoMapper;
+
+    @Autowired
+    private LocalizacaoService localizacaoService;
+
+    @ApiOperation(value = "Retorna um localização")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localização encontrada com sucesso")
+    })
+    @GetMapping("/{id}")
+    public LocalizacaoDTOResponse buscarPor(@PathVariable Long id) throws ObjectNotFoundException {
+        Localizacao localizacao =  localizacaoService.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Localização com id " + id + " não encontrado"));
+        LocalizacaoDTOResponse localizacaoDTOResponse = localizacaoMapper.toDTOResponse(localizacao);
+        return localizacaoDTOResponse;
+    }
+
+    @ApiOperation(value = "Cria um localização")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localização criada com sucesso")
+    })
+    @PostMapping
+    public LocalizacaoDTOResponse criar(@RequestBody @Valid LocalizacaoDTO localizacaoDTO) {
+        Localizacao localizacao = localizacaoMapper.toEntity(localizacaoDTO);
+        localizacaoService.save(localizacao);
+        LocalizacaoDTOResponse localizacaoDTOResponse = localizacaoMapper.toDTOResponse(localizacao);
+        return localizacaoDTOResponse;
+    }
+
+    @ApiOperation(value = "Buscar todas as localizações")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localizações encontradas com sucesso")
+    })
+    @GetMapping("/localizacao")
+    public List<LocalizacaoDTOResponse> buscarTodos() {
+        List<Localizacao> listaLocalizacao = localizacaoService.findAll();
+        return localizacaoMapper.toDTOResponse(listaLocalizacao);
+    }
+
+    @ApiOperation(value = "Deleta uma localização")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Localização deletada com sucesso")
+    })
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) throws ObjectNotFoundException {
+        Localizacao localizacao = localizacaoService.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Localização com id " + id + " não encontrado"));
+        localizacao.setExcluido(Boolean.TRUE);
+        localizacaoService.save(localizacao);
     }
 
 }
--- src/main/java/br/com/academiadev/bumblebee/controller/PetController.java
+++ src/main/java/br/com/academiadev/bumblebee/controller/PetController.java
@@ -1,8 +1,20 @@
 package br.com.academiadev.bumblebee.controller;
 
+import br.com.academiadev.bumblebee.dto.Localizacao.LocalizacaoDTO;
+import br.com.academiadev.bumblebee.dto.Localizacao.LocalizacaoDTOResponse;
+import br.com.academiadev.bumblebee.dto.Pet.PetDTO;
+import br.com.academiadev.bumblebee.dto.Pet.PetDTOResponse;
 import br.com.academiadev.bumblebee.enums.Categoria;
+import br.com.academiadev.bumblebee.exception.ObjectNotFoundException;
+import br.com.academiadev.bumblebee.mapper.FotoPetMapper;
+import br.com.academiadev.bumblebee.mapper.LocalizacaoMapper;
+import br.com.academiadev.bumblebee.mapper.PetMapper;
+import br.com.academiadev.bumblebee.model.Localizacao;
 import br.com.academiadev.bumblebee.model.Pet;
 import br.com.academiadev.bumblebee.model.Usuario;
+import br.com.academiadev.bumblebee.repository.LocalizacaoRepository;
+import br.com.academiadev.bumblebee.repository.PetRepository;
+import br.com.academiadev.bumblebee.service.LocalizacaoService;
 import br.com.academiadev.bumblebee.service.PetService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -11,34 +23,89 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.Api;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/pet")
 @Api(description = "Pets")
-public class PetController extends CrudControllerAbstrato<PetService, Pet, Long> {
+public class PetController{
 
     @Autowired
-    public PetController(PetService service){
-        super(service);
+    private PetRepository petRepository;
+
+    @Autowired
+    private PetMapper petMapper;
+
+    @Autowired
+    private PetService petService;
+
+    @ApiOperation(value = "Retorna um pet")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Pet encontrado com sucesso")
+    })
+    @GetMapping("/{id}")
+    public PetDTOResponse buscarPor(@PathVariable Long id) throws ObjectNotFoundException {
+        Pet pet =  petService.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Pet com id " + id + " não encontrado"));
+        PetDTOResponse petDTOResponse = petMapper.toDTOResponse(pet);
+        return petDTOResponse;
     }
 
-    @ApiOperation(value = "Retorna entidades filtrados por categoria")
+    @ApiOperation(value = "Cria um pet")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Entidades encontradas com sucesso")
+            @ApiResponse(code = 201, message = "Pet criada com sucesso")
+    })
+    @PostMapping
+    public PetDTOResponse criar(@RequestBody @Valid PetDTO petDTO) {
+        Pet pet = petMapper.toEntity(petDTO);
+        Date now = new Date();
+        pet.setDataPostagem(now);
+        petService.save(pet);
+        PetDTOResponse petDTOResponse = petMapper.toDTOResponse(pet);
+        return petDTOResponse;
+    }
+
+    @ApiOperation(value = "Buscar todos as pets")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Pets encontrados com sucesso")
+    })
+    @GetMapping("/pet")
+    public List<PetDTOResponse> buscarTodos() {
+        List<Pet> listaPets = petService.findAll();
+        return petMapper.toDTOResponse(listaPets);
+    }
+
+    @ApiOperation(value = "Deleta um pet")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Pet deletado com sucesso")
+    })
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) throws ObjectNotFoundException {
+        Pet pet = petService.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Pet com id " + id + " não encontrado"));
+        pet.setExcluido(Boolean.TRUE);
+        petService.save(pet);
+    }
+
+    @ApiOperation(value = "Retorna pets filtrados por categoria")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Pets encontrados com sucesso")
     })
     @GetMapping("/categoria/{descricao}")
     public List<Pet> buscarPorCategoria(@PathVariable (value = "descricao") Categoria categoria) {
-        return service.findAllByCategoria(categoria);
+        return petService.findAllByCategoria(categoria);
     }
 
-    @ApiOperation(value = "Retorna entidades filtradas por usuário")
+    @ApiOperation(value = "Retorna pets filtradas por usuário")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Entidades encontradas com sucesso")
+            @ApiResponse(code = 201, message = "Pets encontrados com sucesso")
     })
     @GetMapping("/usuario/{usuario}")
     public List<Pet> buscarPorUsuario(@PathVariable (value = "usuario") Usuario usuario) {
-        return service.findAllByUsuario(usuario);
+        return petService.findAllByUsuario(usuario);
     }
 
 }
