026371bf4957a6ce4718500dc5b5d823f883f58f
viniciusbsi <ifc.vinicius@gmail.com>
2018-11-27 10:06:13 -0200

    #57 - ALterado os mappers de usuário, cidade, localização e uf: criado os métodos de retorar listas de DTOs e mapeamentos de DTO response para DTO request.

--- src/main/java/br/com/academiadev/bumblebee/mapper/LocalizacaoMapper.java
+++ src/main/java/br/com/academiadev/bumblebee/mapper/LocalizacaoMapper.java
@@ -1,21 +1,27 @@
 package br.com.academiadev.bumblebee.mapper;
 
-import br.com.academiadev.bumblebee.dto.LocalizacaoDTO;
+import br.com.academiadev.bumblebee.dto.Cidade.CidadeDTOResponse;
+import br.com.academiadev.bumblebee.dto.Localizacao.LocalizacaoDTO;
+import br.com.academiadev.bumblebee.dto.Localizacao.LocalizacaoDTOResponse;
+import br.com.academiadev.bumblebee.model.Cidade;
 import br.com.academiadev.bumblebee.model.Localizacao;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-public interface LocalizacaoMapper extends EntityMapper<Localizacao, LocalizacaoDTO> {
-    @Mappings({
+import java.util.List;
 
-            @Mapping(source = "createdAt", target = "created_at", dateFormat = "dd/MM/yyyy HH:mm")
-    })
+@Mapper(componentModel = "spring")
+public interface LocalizacaoMapper extends EntityMapper<Localizacao, LocalizacaoDTO> {
+
     @Override
     LocalizacaoDTO toDTO(Localizacao entity);
 
-    @Mappings({
-            @Mapping(target = "createdAt", source = "created_at", dateFormat = "dd/MM/yyyy HH:mm")
-    })
     @Override
     Localizacao toEntity(LocalizacaoDTO localizacaoDTO);
+
+    LocalizacaoDTOResponse toDTOResponse(Localizacao entity);
+
+    List<LocalizacaoDTOResponse> toDTOResponse(List<Localizacao> entity);
+
 }
--- src/main/java/br/com/academiadev/bumblebee/mapper/UsuarioMapper.java
+++ src/main/java/br/com/academiadev/bumblebee/mapper/UsuarioMapper.java
@@ -1,12 +1,13 @@
 package br.com.academiadev.bumblebee.mapper;
 
-import br.com.academiadev.bumblebee.dto.UsuarioDTO;
-import br.com.academiadev.bumblebee.dto.UsuarioDTOResponse;
+import br.com.academiadev.bumblebee.dto.Usuario.UsuarioDTO;
+import br.com.academiadev.bumblebee.dto.Usuario.UsuarioDTOResponse;
 import br.com.academiadev.bumblebee.model.Usuario;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper extends EntityMapper<Usuario, UsuarioDTO> {
@@ -14,6 +15,8 @@ public interface UsuarioMapper extends EntityMapper<Usuario, UsuarioDTO> {
     UsuarioDTO toDTO(Usuario entity);
 
     UsuarioDTOResponse toDTOResponse(Usuario entity);
+
+    List<UsuarioDTOResponse> toDTOResponse(List<Usuario> entity);
 
     @Override
     Usuario toEntity(UsuarioDTO usuarioDTO);
