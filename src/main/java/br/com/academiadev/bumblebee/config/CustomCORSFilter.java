6dbfe5f865d6783f8e68ddc6bc63a0637839059d
Bruno Miguel <bm.morais21@gmail.com>
2018-12-08 10:15:07 -0200

    teste de email

--- src/main/java/br/com/academiadev/bumblebee/config/CustomCORSFilter.java
+++ src/main/java/br/com/academiadev/bumblebee/config/CustomCORSFilter.java
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/*
- * Precisamos implementar este filtro CORS pois a implementação do
- * Spring não habilitava os endpoints do Oauth
- */
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
